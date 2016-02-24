8ed83d58c6e26d90d5be868b765f0041c1a90f93
michaelbal <michael.ball@easyjet.com>
2016-02-24 15:29:59 +0000

    Initial Check In

---
+++ src/GetAsyncSlowIssue/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace GetAsyncSlowIssue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var testClass = new TestHttpClient();
+            var totalDuration = new TimeSpan();
+            var numTests = 10;
+
+            // ignore first run due to 'initialisation' making it slower
+            var temp = Task.Run(() => testClass.Test()).Result;
+            
+            for (var index = 0; index < numTests; index++)
+            {
+                var duration = Task.Run(() => testClass.Test()).Result;
+                
+                Console.WriteLine("{0}: {1} ms", index, duration.TotalMilliseconds);
+
+                totalDuration = totalDuration.Add(duration);
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Total Duration: {0} ms", totalDuration.TotalMilliseconds);
+            Console.WriteLine("Average: {0} ms", totalDuration.TotalMilliseconds / numTests);
+
+            Console.ReadLine();
+        }
+    }
+
+    internal class TestHttpClient
+    {
+        public async Task<TimeSpan> Test()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("test", "v1"));
+            var uri = new Uri("https://api.github.com/repos/dotnet/corefx/issues");
+
+            var startTime = DateTime.Now;
+
+            await httpClient.GetAsync(uri);
+
+            var endTime = DateTime.Now;
+
+            return endTime.Subtract(startTime);
+        }
+    }
+}
